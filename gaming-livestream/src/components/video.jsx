267d25a7cebcc5f19d96dd2971bcfc1a23786e9d
Anders Lund <anderslund@Anderss-MacBook-Pro-2.local>
2020-12-11 09:34:56 -0700

    added video

--- gaming-livestream/src/components/video.jsx
+++ gaming-livestream/src/components/video.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import '../styles/video.scss'
 
-// video file was too big for GH
-// import video from '../assets/videos/livestream.webm'
+import video from '../assets/videos/livestream.webm'
 
 const Video = props => {
 
   return (
-    <video autoPlay muted loop controls className={`video-container ${props.isFullScreen ? 'full-screen' : ''}`}>
-      <source src={''} />
+    <video autoPlay muted loop className={`video-container ${props.isFullScreen ? 'full-screen' : ''}`}>
+      <source src={video} />
     </video>
   );
 }
