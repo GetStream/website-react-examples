dfdfd47fcfd691b327053dd1e3989d6460ae2d82
Dan Carbonell <dan@getstream.io>
2020-12-17 14:14:08 -0700

    more to message component

--- gaming-livestream/src/components/GamingMessage/GamingMessage.js
+++ gaming-livestream/src/components/GamingMessage/GamingMessage.js
@@ -2,17 +2,54 @@ import React from 'react';
 
 import './GamingMessage.scss';
 
+import ActionDownVote from '../../assets/icons/ActionDownVote';
+import ActionThread from '../../assets/icons/ActionThread';
+import ActionUpVote from '../../assets/icons/ActionUpVote';
+import ReactionDownVote from '../../assets/icons/ReactionDownVote';
+import ReactionUpVote from '../../assets/icons/ReactionUpVote';
 import UserIcon from '../../assets/icons/UserIcon';
 import { getColor } from '../../assets/data';
 
 export const GamingMessage = (props) => {
+  const { message } = props;
+
+  // const hasVotes = message.upVotes > 0 || message.downVotes > 0;
+  const hasVotes = true;
+
   return (
-    <div className='custom-message'>
-      <UserIcon />
-      <p className='message-owner' style={{ color: getColor() }}>
-        {props.message.user.id}
-      </p>
-      <p className='message'>{props.message.text}</p>
+    <div className='custom-message__wrapper'>
+      <div className='custom-message__content'>
+        <UserIcon />
+        <p className='message-owner' style={{ color: getColor() }}>
+          {props.message.user.id}
+        </p>
+        <p className='message'>{props.message.text}</p>
+      </div>
+      {hasVotes && (
+        <div className='custom-message__reaction-list'>
+          {/* {message.upVotes > 0 && ( */}
+          {
+            <>
+              <ReactionUpVote />
+              <p>10</p>
+              {/* <p>message.upVotes</p> */}
+            </>
+          }
+          {/* {message.downVotes > 0 && ( */}
+          {
+            <>
+              <ReactionDownVote />
+              <p>8</p>
+              {/* <p>message.downVotes</p> */}
+            </>
+          }
+        </div>
+      )}
+      <div className='custom-message__actions-wrapper'>
+        <ActionUpVote />
+        <ActionDownVote />
+        <ActionThread />
+      </div>
     </div>
   );
 };
