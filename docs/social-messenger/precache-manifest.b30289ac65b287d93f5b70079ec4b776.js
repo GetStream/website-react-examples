72acc163a42f0c833adb6900f729eff04f8b6895
Jaap <jaap@getstream.io>
2020-12-18 16:32:04 +0100

    new social messaging app

---
+++ docs/social-messenger/precache-manifest.b30289ac65b287d93f5b70079ec4b776.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c657578c9efe6767586889152f6c34d8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "761b9cd625a8c54a8bdc",
+    "url": "./static/css/2.a7159ab6.chunk.css"
+  },
+  {
+    "revision": "973f9c2dbd4a464bbc19",
+    "url": "./static/css/main.e7ee6d5e.chunk.css"
+  },
+  {
+    "revision": "761b9cd625a8c54a8bdc",
+    "url": "./static/js/2.589b03de.chunk.js"
+  },
+  {
+    "revision": "a586c0c4dc3e34c5315040fea0d25a53",
+    "url": "./static/js/2.589b03de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "973f9c2dbd4a464bbc19",
+    "url": "./static/js/main.af3b3a2c.chunk.js"
+  },
+  {
+    "revision": "671eb0b01a7567571b62",
+    "url": "./static/js/runtime-main.dd7aa980.js"
+  },
+  {
+    "revision": "ce7bf37000e2a5ecf5fd5741ff012eb0",
+    "url": "./static/media/stream.ce7bf370.png"
+  }
+]);
