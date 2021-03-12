1336ff69327688743c5e64f4b00e86eb77969a24
Anna Baldwin <anna@getstream.io>
2021-03-12 10:47:25 -0700

    starting of team typescripted

---
+++ team-typescript/src/App.tsx
@@ -0,0 +1,97 @@
+// import React, { useEffect, useState } from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat, enTranslations, Streami18n } from 'stream-chat-react';
+// import { createGlobalStyle } from 'styled-components';
+import 'stream-chat-react/dist/css/index.css';
+
+import './App.css';
+
+// import { useChecklist } from './ChecklistTasks';
+// import { ChannelContainer } from './components/ChannelContainer/ChannelContainer';
+// import { ChannelListContainer } from './components/ChannelListContainer/ChannelListContainer';
+// import { ColorSlider } from './components/ColorSlider/ColorSlider';
+
+import { getRandomImage } from './assets';
+
+const urlParams = new URLSearchParams(window.location.search);
+const apiKey = urlParams.get('apikey') || process.env.REACT_APP_STREAM_KEY;
+const user = urlParams.get('user') || process.env.REACT_APP_USER_ID;
+const theme = urlParams.get('theme') || 'light';
+const userToken = urlParams.get('user_token') || process.env.REACT_APP_USER_TOKEN;
+// const targetOrigin = urlParams.get('target_origin') || process.env.REACT_APP_TARGET_ORIGIN;
+
+const i18nInstance = new Streami18n({
+  language: 'en',
+  translationsForLanguage: {
+    ...enTranslations,
+    Flag: 'Pin to Channel',
+  },
+});
+
+// const GlobalColor = createGlobalStyle`
+//   body {
+//     --primary-color: ${(props) => props.color.current};
+//   }
+// `;
+
+// const filters = [{ type: 'team' }, { type: 'messaging' }];
+// const options = { state: true, watch: true, presence: true, limit: 3 };
+// const sort = { last_message_at: -1, updated_at: -1 };
+
+const client = StreamChat.getInstance(apiKey!);
+client.connectUser({ id: user!, name: user, image: getRandomImage() }, userToken);
+
+const App = () => {
+  // const [createType, setCreateType] = useState('');
+  // const [isCreating, setIsCreating] = useState(false);
+  // const [isEditing, setIsEditing] = useState(false);
+
+  // useChecklist(client, targetOrigin);
+  // const primaryColor = useRef('78, 29, 157');
+
+  // useEffect(() => {
+  //   const handleColorChange = (color) => {
+  //     const root = document.documentElement;
+  //     if (color.length && color.length === 7) {
+  //       root.style.setProperty('--primary-color', `${color}E6`);
+  //       root.style.setProperty('--primary-color-alpha', `${color}1A`);
+  //     }
+  //   };
+
+  //   window.addEventListener('message', (event) => handleColorChange(event.data));
+  //   return () => window.removeEventListener('message', (event) => handleColorChange(event.data));
+  // }, []);
+
+  return (
+    <>
+      <div className='app__wrapper'>
+        <Chat {...{ client, i18nInstance }} theme={`team ${theme}`}>
+          <div>Theme IS!: {theme}</div>
+          {/* <ChannelListContainer
+            {...{
+              isCreating,
+              filters,
+              options,
+              setCreateType,
+              setIsCreating,
+              setIsEditing,
+              sort,
+            }}
+          />
+          <ChannelContainer
+            {...{
+              createType,
+              isCreating,
+              isEditing,
+              setIsCreating,
+              setIsEditing,
+            }}
+          /> */}
+        </Chat>
+      </div>
+    </>
+  );
+};
+
+export default App;
+
