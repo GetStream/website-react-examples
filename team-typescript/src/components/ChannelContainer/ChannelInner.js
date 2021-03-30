e3267ed9c237daf499e0602aec54471e6603937a
Anna Baldwin <anna@getstream.io>
2021-03-30 11:15:17 -0600

    all files moved

--- team-typescript/src/components/ChannelContainer/ChannelInner.js
+++ team-typescript/src/components/ChannelContainer/ChannelInner.js
@@ -1,58 +1,57 @@
-// import React, { useState } from 'react';
-// import { MessageList, Thread, Window } from 'stream-chat-react';
-import { Window } from 'stream-chat-react';
+import React, { useState } from 'react';
+import { MessageList, Thread, Window } from 'stream-chat-react';
 
-// import { ChannelEmptyState } from '../ChannelEmptyState/ChannelEmptyState';
-// import { PinnedMessageList } from '../PinnedMessageList/PinnedMessageList';
-// import { TeamChannelHeader } from '../TeamChannelHeader/TeamChannelHeader';
-// import { TeamMessage } from '../TeamMessage/TeamMessage';
-// import { TeamMessageInput } from '../TeamMessageInput/TeamMessageInput';
-// import { ThreadMessageInput } from '../TeamMessageInput/ThreadMessageInput';
+import { ChannelEmptyState } from '../ChannelEmptyState/ChannelEmptyState';
+import { PinnedMessageList } from '../PinnedMessageList/PinnedMessageList';
+import { TeamChannelHeader } from '../TeamChannelHeader/TeamChannelHeader';
+import { TeamMessage } from '../TeamMessage/TeamMessage';
+import { TeamMessageInput } from '../TeamMessageInput/TeamMessageInput';
+import { ThreadMessageInput } from '../TeamMessageInput/ThreadMessageInput';
 
-// import { CloseThreadIcon } from '../../assets';
+import { CloseThreadIcon } from '../../assets';
 
-// const ThreadHeader = ({ closeThread, setPinsOpen, thread }) => {
-//   const getReplyCount = () => {
-//     if (!thread?.reply_count) return '';
-//     if (thread.reply_count === 1) return '1 reply';
-//     return `${thread.reply_count} Replies`;
-//   };
+const ThreadHeader = ({ closeThread, setPinsOpen, thread }) => {
+  const getReplyCount = () => {
+    if (!thread?.reply_count) return '';
+    if (thread.reply_count === 1) return '1 reply';
+    return `${thread.reply_count} Replies`;
+  };
 
-//   return (
-//     <div className='custom-thread-header'>
-//       <div className='custom-thread-header__left'>
-//         <p className='custom-thread-header__left-title'>Thread</p>
-//         <p className='custom-thread-header__left-count'>{getReplyCount()}</p>
-//       </div>
-//       <CloseThreadIcon {...{ closeThread, setPinsOpen }} />
-//     </div>
-//   );
-// };
+  return (
+    <div className='custom-thread-header'>
+      <div className='custom-thread-header__left'>
+        <p className='custom-thread-header__left-title'>Thread</p>
+        <p className='custom-thread-header__left-count'>{getReplyCount()}</p>
+      </div>
+      <CloseThreadIcon {...{ closeThread, setPinsOpen }} />
+    </div>
+  );
+};
 
 export const ChannelInner = (props) => {
-  // const { pinsOpen, setIsEditing, setPinsOpen } = props;
+  const { pinsOpen, setIsEditing, setPinsOpen } = props;
 
-  // const [pinnedMessages, setPinnedMessages] = useState({});
-  // const pinnedMessagesIds = Object.keys(pinnedMessages);
+  const [pinnedMessages, setPinnedMessages] = useState({});
+  const pinnedMessagesIds = Object.keys(pinnedMessages);
 
   return (
     <div style={{ display: 'flex', width: '100%' }}>
       <Window>
-        {/* <TeamChannelHeader {...{ setIsEditing, setPinsOpen }} />
+        <TeamChannelHeader {...{ setIsEditing, setPinsOpen }} />
         <MessageList
           EmptyStateIndicator={ChannelEmptyState}
           Message={(messageProps) => <TeamMessage {...messageProps} {...{ pinnedMessagesIds, setPinnedMessages, setPinsOpen }} />}
           TypingIndicator={() => null}
         />
-        <TeamMessageInput focus {...{ pinsOpen }} /> */}
+        <TeamMessageInput focus {...{ pinsOpen }} />
       </Window>
-      {/* <Thread
+      <Thread
         additionalMessageListProps={{ TypingIndicator: () => null }}
         Message={TeamMessage}
         MessageInput={ThreadMessageInput}
         ThreadHeader={(threadProps) => <ThreadHeader {...threadProps} {...{ setPinsOpen }} />}
-      /> */}
-      {/* {pinsOpen && <PinnedMessageList {...{ pinnedMessages, setPinsOpen }} />} */}
+      />
+      {pinsOpen && <PinnedMessageList {...{ pinnedMessages, setPinsOpen }} />}
     </div>
   );
 };
