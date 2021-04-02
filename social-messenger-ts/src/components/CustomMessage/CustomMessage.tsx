9cc80725b1c5fd253aa826eb2e5fbe22a529ea15
Grant Steinke <grantjsteinke@gmail.com>
2021-04-02 12:36:45 -0600

    CustomMessage TS

---
+++ social-messenger-ts/src/components/CustomMessage/CustomMessage.tsx
@@ -0,0 +1,13 @@
+import { MessageUIComponentProps, MessageSimple } from 'stream-chat-react';
+
+import './CustomMessage.css';
+
+const CustomMessage = (props: MessageUIComponentProps) => {
+  return (
+    <>
+      <MessageSimple {...props} />
+    </>
+  );
+};
+
+export default CustomMessage;
