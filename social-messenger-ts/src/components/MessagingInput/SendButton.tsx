669ae424f7eb333d3fda59113adc2c16efb6b1ad
MartinCupela <martin.cupela@gmail.com>
2022-09-28 14:01:37 +0200

    feat(social-messenger-ts): add custom SendButton

---
+++ social-messenger-ts/src/components/MessagingInput/SendButton.tsx
@@ -0,0 +1,14 @@
+import { SendIcon } from '../../assets';
+import React from 'react';
+import type { SendButtonProps } from 'stream-chat-react';
+
+export const SendButton = ({sendMessage, ...rest}: SendButtonProps) => (
+  <button
+    className='str-chat__send-button'
+    onClick={sendMessage}
+    type='button'
+    {...rest}
+  >
+    <SendIcon />
+  </button>
+)
