d82b7614e6d2049fef70df249950e8e9a2d30cb7
MartinCupela <martin.cupela@gmail.com>
2022-09-28 13:26:33 +0200

    feat(social-messenger-ts): use theme V2 theme names

--- social-messenger-ts/src/hooks/useTheme.ts
+++ social-messenger-ts/src/hooks/useTheme.ts
@@ -1,17 +1,24 @@
 import { useEffect, useState } from 'react';
 
+export type Theme = 'str-chat__theme-light' | 'str-chat__theme-dark';
+
+const SUPPORTED_THEMES = [
+  'str-chat__theme-light',
+  'str-chat__theme-dark'
+] as const;
+
 /**
  * Internal, handles the communication between Stream's website related to UI theme toggling.
  *
  * @param targetOrigin the target origin (typically, the https://getstream.io/ domain).
  */
 export const useTheme = (targetOrigin: string) => {
-  const [theme, setTheme] = useState<'light' | 'dark'>('dark');
+  const [theme, setTheme] = useState<Theme>('str-chat__theme-light');
   useEffect(() => {
-    const handleThemeChange = ({ data, origin }: MessageEvent<'light' | 'dark'>) => {
+    const handleThemeChange = ({ data: theme, origin }: MessageEvent<Theme>) => {
       // handle events only from trusted origin
-      if (origin === targetOrigin && (data === 'light' || data === 'dark')) {
-        setTheme(data);
+      if (origin === targetOrigin && SUPPORTED_THEMES.includes(theme)) {
+        setTheme(theme);
       }
     };
 
