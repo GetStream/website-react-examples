c63e561f73c113fd2690854472eb006fafd7db5f
MartinCupela <martin.cupela@gmail.com>
2022-09-28 14:03:38 +0200

    fix(social-messenger-ts): adjust component exports

--- social-messenger-ts/src/components/index.ts
+++ social-messenger-ts/src/components/index.ts
@@ -1,10 +1,9 @@
 export { default as AvatarGroup } from './AvatarGroup/AvatarGroup';
 export { default as CreateChannel } from './CreateChannel/CreateChannel';
-export { default as CustomMessage } from './CustomMessage/CustomMessage';
 export { default as MessagingChannelHeader } from './MessagingChannelHeader/MessagingChannelHeader';
 export { default as MessagingChannelList } from './MessagingChannelList/MessagingChannelList';
 export { default as MessagingChannelListHeader } from './MessagingChannelList/MessagingChannelListHeader';
 export { default as MessagingChannelPreview } from './MessagingChannelPreview/MessagingChannelPreview';
-export { default as MessagingInput } from './MessagingInput/MessagingInput';
-export { default as MessagingThreadHeader } from './MessagingThread/MessagingThread';
+export * from './MessagingInput';
+export { default as MessagingThreadHeader } from './MessagingThreadHeader/MessagingThreadHeader';
 export { default as WindowControls } from './WindowControls/WindowControls';
