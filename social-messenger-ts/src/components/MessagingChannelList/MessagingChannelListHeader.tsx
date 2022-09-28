3561e073853bf3a0d95d89b471adb365b4ac1ddb
MartinCupela <martin.cupela@gmail.com>
2022-09-28 13:44:33 +0200

    refactor(social-messenger-ts): remove unnecessary prop theme from MessagingChannelListHeader

--- social-messenger-ts/src/components/MessagingChannelList/MessagingChannelListHeader.tsx
+++ social-messenger-ts/src/components/MessagingChannelList/MessagingChannelListHeader.tsx
@@ -8,29 +8,26 @@ import type { StreamChatGenerics } from '../../types';
 
 type Props = {
   onCreateChannel?: () => void;
-  theme: string;
 };
 
 const MessagingChannelListHeader = React.memo((props: Props) => {
-  const { onCreateChannel, theme } = props;
+  const { onCreateChannel } = props;
 
   const { client } = useChatContext<StreamChatGenerics>();
 
   const { id, image = streamLogo as string, name = 'Example User' } = client.user || {};
 
   return (
-    <div className={`${theme} messaging__channel-list`}>
       <div className='messaging__channel-list__header'>
         <Avatar image={image} name={name} size={40} />
-        <div className={`${theme} messaging__channel-list__header__name`}>{name || id}</div>
+        <div className={`messaging__channel-list__header__name`}>{name || id}</div>
         <button
-          className={`${theme} messaging__channel-list__header__button`}
+          className={`messaging__channel-list__header__button`}
           onClick={onCreateChannel}
         >
           <CreateChannelIcon />
         </button>
       </div>
-    </div>
   );
 });
 
