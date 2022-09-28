a4d754dc66f4c84e10fb482fb12162a9e902863a
MartinCupela <martin.cupela@gmail.com>
2022-09-28 15:37:52 +0200

    feat(social-messenger-ts): optimize AvatarGroup styling

--- social-messenger-ts/src/components/AvatarGroup/AvatarGroup.tsx
+++ social-messenger-ts/src/components/AvatarGroup/AvatarGroup.tsx
@@ -1,65 +1,63 @@
+import React from 'react';
 import type { ChannelMemberResponse } from 'stream-chat';
 import { Avatar } from 'stream-chat-react';
 import { getCleanImage } from '../../assets';
 import './AvatarGroup.css';
 import StreamLogo from '../../assets/stream.png';
-import React from 'react';
 
 export const AvatarGroup = ({ members }: { members: ChannelMemberResponse[] }) => {
+  let content = <Avatar image={StreamLogo} shape='square' size={40} />;
+
   if (members.length === 1) {
-    return (
-      <div className='avatar-group__avatars'>
-        <Avatar image={getCleanImage(members[0])} size={40} />
-      </div>
-    );
+    content = <Avatar image={getCleanImage(members[0])} size={40} />;
   }
 
   if (members.length === 2) {
-    return (
-      <div className='avatar-group__avatars two'>
-        <span>
+    content = (
+      <>
+        <div>
           <Avatar image={getCleanImage(members[0])} shape='square' size={40} />
-        </span>
-        <span>
+        </div>
+        <div>
           <Avatar image={getCleanImage(members[1])} shape='square' size={40} />
-        </span>
-      </div>
+        </div>
+      </>
     );
   }
 
   if (members.length === 3) {
-    return (
-      <div className='avatar-group__avatars three'>
-        <span>
+    content = (
+      <>
+        <div>
           <Avatar image={getCleanImage(members[0])} shape='square' size={40} />
-        </span>
-        <span>
+        </div>
+        <div>
           <Avatar image={getCleanImage(members[1])} shape='square' size={20} />
           <Avatar image={getCleanImage(members[2])} shape='square' size={20} />
-        </span>
-      </div>
+        </div>
+      </>
     );
   }
 
   if (members.length >= 4) {
-    return (
-      <div className='avatar-group__avatars four'>
-        <span>
-          <Avatar image={getCleanImage(members[members.length - 1])} shape='square' size={20} />
-          <Avatar image={getCleanImage(members[members.length - 2])} shape='square' size={20} />
-        </span>
-        <span>
-          <Avatar image={getCleanImage(members[members.length - 3])} shape='square' size={20} />
+    content = (
+      <>
+        <div>
           <Avatar image={getCleanImage(members[members.length - 4])} shape='square' size={20} />
-        </span>
-      </div>
+          <Avatar image={getCleanImage(members[members.length - 3])} shape='square' size={20} />
+        </div>
+        <div>
+          <Avatar image={getCleanImage(members[members.length - 2])} shape='square' size={20} />
+          <Avatar image={getCleanImage(members[members.length - 1])} shape='square' size={20} />
+        </div>
+      </>
     );
   }
 
   // fallback for channels with no avatars (single-user channels)
   return (
-    <div className='avatar-group__avatars'>
-      <Avatar image={StreamLogo} shape='square' size={40} />
+    <div className='avatar-group'>
+      {content}
     </div>
   );
 };
