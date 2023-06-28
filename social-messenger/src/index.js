6506ea4b9bf8548e07eb990763364fe717108deb
Jaap <jaap@getstream.io>
2023-06-28 15:12:37 +0200

    add sentry to social app

--- social-messenger/src/index.js
+++ social-messenger/src/index.js
@@ -3,8 +3,15 @@ import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+Sentry.init({
+  dsn: 'https://cfe9b30508b44e40908da83aee0743e9@o389650.ingest.sentry.io/5556314',
+  integrations: [new Sentry.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
