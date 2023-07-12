e80049f43b0961a93cc1e585245d99428e498698
jaapbakker88 <jaap@getstream.io>
2023-07-12 13:17:11 +0200

    [WD-672]: don't include sentry during development (#79)
    * only load sentry in prod
    
    * yarn build

--- gaming-livestream/src/index.tsx
+++ gaming-livestream/src/index.tsx
@@ -6,11 +6,13 @@ import './styles/index.scss';
 
 import App from './App';
 
-Sentry.init({
-  dsn: 'https://cfe9b30508b44e40908da83aee0743e9@o389650.ingest.sentry.io/5556314',
-  integrations: [new Sentry.BrowserTracing()],
-  tracesSampleRate: 1.0,
-});
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://cfe9b30508b44e40908da83aee0743e9@o389650.ingest.sentry.io/5556314',
+    integrations: [new Sentry.BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+}
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
